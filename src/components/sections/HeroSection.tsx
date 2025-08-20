8e047ae452b1415a0c4dcf37ccd7393f208601c5
Ahmed Osman <Ahmedot2@gmail.com>
2025-08-20 19:20:08 +0000

    reduce it more

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.playbackRate = 0.75;
+      videoRef.current.playbackRate = 0.5;
     }
   }, []);
 
